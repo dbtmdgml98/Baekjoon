b7222ec3763bceced437f380e4a4de4574c8ee20
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-19 15:28:38 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int year = 2022;
+        int result = year - age + 1;
+        return result;
+        
+        //System.out.print("2022년 기준 " + age + "살이므로 " + result + "년생입니다.");
+    }
+}
