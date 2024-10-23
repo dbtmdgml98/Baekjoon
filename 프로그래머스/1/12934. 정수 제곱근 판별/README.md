57a7523e95bf4429df7c420eb1a943eae2ec734f
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-23 10:17:16 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 17.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/README.md
+++ 프로그래머스/1/12934. 정수 제곱근 판별/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 제곱근 판별 - 12934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12934) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12934?language=java) 
 
 ### 성능 요약
 
-메모리: 66 MB, 시간: 19.56 ms
+메모리: 73 MB, 시간: 17.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 10:15:25
+2024년 10월 23일 10:17:13
 
 ### 문제 설명
 
--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,13 +1,14 @@
 class Solution {
-    public long solution(long n) {
-        long answer = 0;
-        
-        for(long i=1; i*i<=n; i++){
-            if(i*i==n){
-                return answer = (i+1)*(i+1);
-            }
-        }
-        answer = -1;
-        return answer;
-    }
+  public long solution(long n) {
+      long answer = 0;
+
+      for (long i = 1; i <= n; i++) {
+          if (i * i == n) {
+              answer = (i + 1) * (i + 1);
+              break;
+          }
+          else answer = -1;
+      }
+      return answer;
+  }
 }
