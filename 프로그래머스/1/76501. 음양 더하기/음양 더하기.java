4f08a28bed4fe858f62a7a32914cafb508fc92b6
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-27 19:18:35 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,16 +2,12 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for (int i = 0; i < signs.length; i++){
+        for (int i = 0; i < signs.length; i++) {
             if (signs[i] == false) {
             absolutes[i] *= -1;
             }
-        }
-        
-        for (int i = 0; i < signs.length; i++) {
             answer += absolutes[i];
         }
-        
         return answer;
     }
 }
