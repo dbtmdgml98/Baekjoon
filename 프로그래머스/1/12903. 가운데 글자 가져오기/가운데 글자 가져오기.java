66ea791a78e7c4a99b5f3c39bb9d5223dd82b987
dbtmdgml98 <dbtmdgml98@naver.com>
2024-11-19 16:10:33 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        
-        if (s.length() % 2 != 0) {
-            answer = Character.toString(s.charAt(s.length()/2));
-        } else {
-            answer = Character.toString(s.charAt(s.length()/2 - 1));
-            answer += Character.toString(s.charAt(s.length()/2));
-        }
-        return answer;
+        return s.substring((s.length()-1)/2, s.length()/2 + 1);
     }
 }
